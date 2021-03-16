482eefebc5f639d9494c33830f6b7ae9b1b1ff36
Kurler3 <miguelbento2000@gmail.com>
2021-03-16 21:42:07 +0800

    Continuation of the front end project with Scss

---
+++ FronEndMentorChallenges/BlogrLandingPage/dist/js/main.js
@@ -0,0 +1,18 @@
+const mobileMenuBtn = document.querySelector('#nav-menu-btn');
+
+const main = document.querySelector('main');
+
+const footer = document.querySelector('footer');
+
+let isMobileMenuOpen = false;
+
+mobileMenuBtn.addEventListener('click', e => {
+  // If the menu is open then we have to close it, change the icon and display main and footer again. 
+  if(isMobileMenuOpen){
+
+  
+  // If the menu is closed, then we want to open it, change the icon and change the display of main and footer to none.
+  }else {
+
+  }
+})
