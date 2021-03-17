8364ad468219b0925e615781b3e068603e24d401
Kurler3 <miguelbento2000@gmail.com>
2021-03-17 14:26:16 +0800

    Adapting design for desktop

--- FronEndMentorChallenges/BlogrLandingPage/dist/js/main.js
+++ FronEndMentorChallenges/BlogrLandingPage/dist/js/main.js
@@ -2,11 +2,20 @@ const mobileMenuBtn = document.querySelector('#nav-menu-btn');
 
 const mobileMenuContainer = document.querySelector('.mobile-menu-container');
 
+
+const mobileNavMenuItems = document.querySelectorAll('.mobile-menu-item');
+
+
+
 const main = document.querySelector('main');
 
 const footer = document.querySelector('footer');
 
+
+
 let isMobileMenuOpen = false;
+
+let currentMobileNavMenuOpenID;
 
 mobileMenuBtn.addEventListener('click', e => {
   // If the menu is open then we have to close it, change the icon and display main and footer again. 
@@ -42,3 +51,41 @@ mobileMenuBtn.addEventListener('click', e => {
     isMobileMenuOpen = true;
   }
 })
+
+mobileNavMenuItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+
+    // Close the extra if clicked on the same and it was open
+    if(item.id==currentMobileNavMenuOpenID) {
+      item.childNodes[3].style.display = 'none';
+
+      item.children[0].style.color = 'hsl(208, 49%, 24%)';
+      item.children[0].children[1].classList.remove('fa-chevron-up');
+      item.children[0].children[1].classList.add('fa-chevron-down');
+      
+      currentMobileNavMenuOpenID = null;
+    }else{
+      // Close the current item open and open this one
+      if(currentMobileNavMenuOpenID!=null){
+
+        mobileNavMenuItems.forEach((itemLoop) => {
+          if(itemLoop.id == currentMobileNavMenuOpenID) {
+            itemLoop.childNodes[3].style.display = 'none';
+
+            item.children[0].style.color = 'hsl(208, 49%, 24%)';
+            item.children[0].children[1].classList.remove('fa-chevron-up');
+            item.children[0].children[1].classList.add('fa-chevron-down');
+          }
+        })
+      }
+      
+      currentMobileNavMenuOpenID = item.id;
+
+      item.childNodes[3].style.display = 'block';
+
+      item.children[0].style.color = 'grey';
+      item.children[0].children[1].classList.add('fa-chevron-up');
+      item.children[0].children[1].classList.remove('fa-chevron-down');
+    }
+  });
+});
