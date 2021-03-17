ae4a03494cf01d161e3c39e1698696d12e4e86c4
Kurler3 <miguelbento2000@gmail.com>
2021-03-17 09:53:03 +0800

    Almost finished with the mobile design for the project using scss

--- FronEndMentorChallenges/BlogrLandingPage/dist/js/main.js
+++ FronEndMentorChallenges/BlogrLandingPage/dist/js/main.js
@@ -1,5 +1,7 @@
 const mobileMenuBtn = document.querySelector('#nav-menu-btn');
 
+const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+
 const main = document.querySelector('main');
 
 const footer = document.querySelector('footer');
@@ -9,10 +11,34 @@ let isMobileMenuOpen = false;
 mobileMenuBtn.addEventListener('click', e => {
   // If the menu is open then we have to close it, change the icon and display main and footer again. 
   if(isMobileMenuOpen){
+// Change the btn to a cross
+    mobileMenuBtn.classList.remove('fa-times');
+    mobileMenuBtn.classList.add('fa-bars');
 
+    // Make the container appear
+    mobileMenuContainer.style.display = 'none';
+
+    // Make main and footer disappear
+    main.style.display = 'block';
+    footer.style.display = 'flex';
+
+    // Update the isOpen variable
+    isMobileMenuOpen = false;
   
   // If the menu is closed, then we want to open it, change the icon and change the display of main and footer to none.
   }else {
+    // Change the btn to a cross
+    mobileMenuBtn.classList.remove('fa-bars');
+    mobileMenuBtn.classList.add('fa-times');
 
+    // Make the container appear
+    mobileMenuContainer.style.display = 'block';
+  
+    // Make main and footer disappear
+    main.style.display = 'none';
+    footer.style.display = 'none';
+
+    // Update the isOpen variable
+    isMobileMenuOpen = true;
   }
 })
